934b36680f1a83e0811520f5146fd8c8b0231b67
katyacyfra <31743249+katyacyfra@users.noreply.github.com>
2018-05-03 23:39:38 +0300

    Revert "Main page + part of sign"
    This reverts commit f07383c8a1b590051d000bdd8c46f22da5c2e045.

--- ChannelViewer/ChannelViewer/urls.py
+++ ChannelViewer/ChannelViewer/urls.py
@@ -3,15 +3,12 @@ from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
 from django.views.defaults import page_not_found
-from django.views.generic.base import RedirectView
 
 from . import views
 
 urlpatterns = [
     url(r'^channel_viewer/', include('channel_viewer.urls')),
     url(r'^$', views.index, name='index'),
-	url(r'^login$', views.sign_in, name='sign_in'),
-	url(r'^favicon\.ico$',RedirectView.as_view(url='/static/images/favicon.ico')),
     #url(r'^admin/', include(admin.site.urls)),
     url(r'^404/$', page_not_found, {'exception ': Exception()}),
 
