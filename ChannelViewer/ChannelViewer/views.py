dfa16a29d014a5ae0ef3549a5d420dde1d8c4bd4
katyacyfra <31743249+katyacyfra@users.noreply.github.com>
2018-05-03 13:28:02 +0300

    Working hello world
    Working hello world

---
+++ ChannelViewer/ChannelViewer/views.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.http import HttpResponse, HttpRequest
+from django.shortcuts import render_to_response, redirect, render
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    name = 'View Channel'
+    return render_to_response('index.html', {'name': name})
