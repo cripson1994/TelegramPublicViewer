f07383c8a1b590051d000bdd8c46f22da5c2e045
katyacyfra <31743249+katyacyfra@users.noreply.github.com>
2018-05-03 23:34:20 +0300

    Main page + part of sign

--- ChannelViewer/ChannelViewer/views.py
+++ ChannelViewer/ChannelViewer/views.py
@@ -7,3 +7,6 @@ from django.shortcuts import render_to_response, redirect, render
 def index(request: HttpRequest) -> HttpResponse:
     name = 'View Channel'
     return render_to_response('index.html', {'name': name})
+
+def sign_in(request: HttpRequest) -> HttpResponse:
+    return render_to_response('signin.html')
