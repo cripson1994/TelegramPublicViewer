5d09f03516b20baf3a17113ee528d8253180d99f
Lara Kolesnichenko <larkkinn@gmail.com>
2018-05-03 15:20:14 +0300

    add login template

--- ChannelViewer/channel_viewer/urls.py
+++ ChannelViewer/channel_viewer/urls.py
@@ -5,3 +5,13 @@ from . import views
 urlpatterns = [
     #url(r'^$', views.main, name='index'),
 ]
+
+from django.conf.urls import url
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^logout/$', auth_views.logout, name='logout'),
+    url(r'^admin/', admin.site.urls),
+]
