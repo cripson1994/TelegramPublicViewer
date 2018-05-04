781cc74ddfc4a29565794dd1e2e71a1b141d27b8
Lara Kolesnichenko <larkkinn@gmail.com>
2018-05-04 13:21:01 +0300

    add signup form

--- ChannelViewer/ChannelViewer/views.py
+++ ChannelViewer/ChannelViewer/views.py
@@ -3,6 +3,8 @@ from __future__ import unicode_literals
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render_to_response, redirect, render
 from unittest.mock import MagicMock
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
 
 class ApiClass:
 	pass
@@ -21,3 +23,17 @@ def channel_posts(request: HttpRequest) -> HttpResponse:
 	
 def login(request: HttpRequest) -> HttpResponse:
     return render_to_response('signin.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
--- ChannelViewer/channel_viewer/urls.py
+++ ChannelViewer/channel_viewer/urls.py
@@ -11,7 +11,5 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    url(r'^login/$', auth_views.login, name='login'),
-    url(r'^logout/$', auth_views.logout, name='logout'),
     url(r'^admin/', admin.site.urls),
 ]
